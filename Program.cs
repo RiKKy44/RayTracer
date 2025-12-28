2a0995f055de432d3853161aa11dcccb7d66c30e
Kacper Frączek <kacper@tee.pl>
2025-12-28 16:59:50 +0100

    update gitignore and create .ppm writer

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
+namespace RayTracer;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int width = 256;
+        const int height = 256;
+
+        using (StreamWriter writer = new StreamWriter("image.ppm"))
+        {
+            writer.WriteLine("P3");
+            writer.WriteLine($"{width} {height}");
+            writer.WriteLine("255");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var r = (double)i / (width - 1);
+                    var g = (double)j / (height - 1);
+                    var b = 0.0;
+
+                    int ir = (int)(255.999 * r);
+                    int ig = (int)(255.999 * g);
+                    int bl = (int)(255.999 * b);
+                    
+                    writer.WriteLine($"{ir} {ig} {bl}");
+                }
+            }
+        }
+    }
+    
+}
