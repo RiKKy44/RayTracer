cdb05ca1da6dfeb01d8101b1de1eb373f1e112ab
Kacper Frączek <kacper@tee.pl>
2025-12-29 16:58:52 +0100

    implement Ray struct with P(t) function

---
+++ GlobalUsings.cs
@@ -0,0 +1,2 @@
+global using Color = RayTracer.Vec3;
+global using Point3 = RayTracer.Vec3;
---
+++ Ray.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace RayTracer;
+
+public readonly struct Ray
+{
+    public Point3 Origin { get; }
+    public Vec3 Direction { get; }
+    
+    public Ray(in Point3 origin, in Vec3 direction)
+    { 
+        Origin = origin;
+        Direction = direction;
+    }
+
+    public Point3 At(double time)
+    {
+        return Origin + Direction * time;
+    }
+    
+    
+}
