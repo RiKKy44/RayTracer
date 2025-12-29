bd24916afc71eaf26685e5600ea4066e3e713480
Kacper Frączek <kacper@tee.pl>
2025-12-29 15:25:21 +0100

    add Vec3 struct and vector math operations

---
+++ Vec3.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace RayTracer;
+
+
+public readonly struct Vec3
+{
+    public double X { get; }
+    public double Y { get; }
+    public double Z { get; }
+
+    public Vec3(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Vec3()
+    {
+        X = 0;
+        Y = 0;
+        Z = 0;
+    }
+
+    public double this[int index] => index switch
+    {
+        0 => X,
+        1 => Y,
+        2 => Z,
+        _ => throw new IndexOutOfRangeException()
+    };
+
+    public static Vec3 operator -(Vec3 v)=> new Vec3(-v.X, -v.Y, -v.Z);
+    
+    public static Vec3 operator +(Vec3 v1, Vec3 v2) => new Vec3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+    
+    public static Vec3 operator -(Vec3 v1, Vec3 v2)=> new Vec3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+    //scalar multiplication
+    public static Vec3 operator *(Vec3 v, double s) => new Vec3(v.X * s, v.Y * s, v.Z * s);
+    
+    //commutative scalar multiplication
+    public static Vec3 operator *(double s, Vec3 v) => v * s;
+    
+    public static Vec3 operator /(Vec3 v, double s) => new Vec3(v.X / s, v.Y / s, v.Z / s);
+    //hadamard product
+    public static Vec3 operator *(Vec3 v1, Vec3 v2) => new Vec3(v1.X*v2.X, v1.Y*v2.Y, v1.Z*v2.Z);
+    public double Length() => Math.Sqrt(X * X + Y * Y + Z * Z);
+    
+    public double LengthSquared() => X * X + Y * Y + Z * Z;
+    
+    public static double Dot(Vec3 v1, Vec3 v2) => v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z;
+    
+    public static Vec3 Cross(Vec3 v1, Vec3 v2) =>
+        new(v1.Y * v2.Z - v1.Z * v2.Y,
+            v1.Z * v2.X - v1.X * v2.Z,
+            v1.X * v2.Y - v1.Y * v2.X);
+
+    public static Vec3 UnitVector(Vec3 v) => v / v.Length();
+
+    public override string ToString() => $"{X} {Y} {Z}";    
+    
+
+}
