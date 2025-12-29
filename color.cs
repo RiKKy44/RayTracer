12700822d0a69f65ca10e470f78fae9b733ced53
Kacper Frączek <kacper@tee.pl>
2025-12-29 15:40:19 +0100

    add Color helper class with WriteColor function

---
+++ color.cs
@@ -0,0 +1,21 @@
+using System.IO;
+namespace RayTracer;
+
+
+public static class Color
+{
+    public static void WriteColor(StreamWriter writer, Vec3 color)
+    {
+        double r = color.X;
+        double g = color.Y;
+        double b = color.Z;
+        
+        int rbyte = (int)(r*255.999);
+        int gbyte = (int)(g*255.999);
+        int bbyte = (int)(b*255.999);
+        
+        writer.WriteLine($"{rbyte} {gbyte} {bbyte}\n");
+        
+    }
+} 
+
