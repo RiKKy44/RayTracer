456bba1013e97aef8e213249f733df19b6c96d16
Kacper Fraczek <kacperxl269@gmail.com>
2026-01-02 21:32:23 +0100

    generating 3 spheres with different materials

--- Program.cs
+++ Program.cs
@@ -10,14 +10,32 @@ public class Program
    public static void Main()
     {
         HittableList world = new HittableList();
-        world.Add(new Sphere(new Point3(0, 0, -1), 0.5));
-        world.Add(new Sphere(new Point3(0, -100.5, -1), 100));
+
+
+        var materialGround = new Lambertian(new Color3(0.8, 0.8, 0.0));
+
+        var materialCenter = new Lambertian(new Color3(0.1, 0.2, 0.5));
+
+        var materialLeft = new Metal(new Color3(0.8, 0.8, 0.8));
+
+        var materialRight = new Metal(new Color3(0.8, 0.6, 0.2));
+
+        world.Add(new Sphere(new Point3(0.0, -100.5, -1.0), 100.0, materialGround));
+        world.Add(new Sphere(new Point3(0.0, 0.0, -1.2), 0.5, materialCenter));
+        world.Add(new Sphere(new Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+        world.Add(new Sphere(new Point3(1.0, 0.0, -1.0), 0.5, materialRight));
+
+
+
+
+
 
         Camera cam = new Camera();
 
         cam.AspectRatio = 16.0 / 9.0;
         cam.ImageWidth = 400;
         cam.SamplesPerPixel = 100;
+        cam.MaxDepth = 50;
         cam.Render(world);
     }
     
